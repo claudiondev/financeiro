4f150f75046bf5fd032e70563d4d50ceba5dad1d
Claudiondev <claudinhon152@gmail.com>
2026-03-10 09:11:11 -0300

    "adiciona entidade Gasto"

---
+++ src/main/java/com/claudio/financeiro/Gasto.java
@@ -0,0 +1,47 @@
+package com.claudio.financeiro;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "gastos")
+
+public class Gasto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private Double valor;
+    private String categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
