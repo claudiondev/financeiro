34f60e510de33e38f0e3a8f7f8bbb64d1d57554a
Claudiondev <claudinhon152@gmail.com>
2026-03-12 17:22:21 -0300

    "adiciona AuthController e SecurityConfig"

---
+++ src/main/java/com/claudio/financeiro/AuthController.java
@@ -0,0 +1,28 @@
+package com.claudio.financeiro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/registrar")
+    public String registrar(@RequestBody Usuario usuario) {usuario.setSenha(passwordEncoder.encode(usuario.getSenha())); usuarioRepository.save(usuario);
+        return "Usuário registrado com sucesso!";
+    }
+}
+
