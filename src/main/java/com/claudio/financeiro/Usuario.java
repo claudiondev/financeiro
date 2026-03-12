517bac345bfb040831e52e722b5fa13023520c04
Claudiondev <claudinhon152@gmail.com>
2026-03-12 16:19:34 -0300

    "adiciona Spring Security"

---
+++ src/main/java/com/claudio/financeiro/Usuario.java
@@ -0,0 +1,44 @@
+package com.claudio.financeiro;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "usuarios")
+
+
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String email;
+    private String senha;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+
+}
